b98c9e1379fc75c9a5e07b78eeefee785188c221
JSicilia <99730755+JSicilia@users.noreply.github.com>
2022-06-14 22:35:26 +0100

    Bug fixes

--- Assets/Scripts/WaypointFinder.cs
+++ Assets/Scripts/WaypointFinder.cs
@@ -8,6 +8,21 @@ public class WaypointFinder : MonoBehaviour
     public List<Vector3> waypoints;
     public float Speed;
     private int blockPoint;
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.collider.transform.SetParent(transform);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        collision.collider.transform.SetParent(null);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
